400e91e77c5e11ef21e6ba1e1a76d54c78f19fe1
Rishi556 <rishipanthee@hotmail.com>
2022-08-11 08:24:02 -0400

    Use IPv6 compatible pgp mongo domain

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ sudo npm install -g npm
 ##########################
 ## Install Dependencies ##
 ##########################
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+wget -qO - https://pgp.mongodb.com/server-5.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 sudo apt update -y
 sudo apt install git -y
